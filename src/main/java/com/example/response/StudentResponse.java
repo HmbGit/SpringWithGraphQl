72fcb7fffad6c42cc553b5142f46fc4e363a606f
Haithem <bobhaytem@gmail.com>
2023-08-03 15:22:07 +0100

    refactor: update query graphQL && studentResponse

--- src/main/java/com/example/response/StudentResponse.java
+++ src/main/java/com/example/response/StudentResponse.java
@@ -1,8 +1,11 @@
 package com.example.response;
 
 import com.example.domain.Student;
+import com.example.domain.Subjects;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,11 +21,16 @@ public class StudentResponse {
 
     private int age;
 
+    private List<SubjectsResponse> listSubjects;
+
+    private Student student;
+
     public StudentResponse(Student student){
         this.id =student.getId();
         this.firstName =student.getFirstName();
         this.lastName =student.getLastName();
         this.address =student.getAddress();
         this.age =student.getAge();
+        this.student = student;
     }
 }
