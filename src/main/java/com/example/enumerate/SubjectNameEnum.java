7280a349de7ee8ce19317f334176d78c18267359
Haithem <bobhaytem@gmail.com>
2023-08-04 16:23:24 +0100

    add enum and filter subject

---
+++ src/main/java/com/example/enumerate/SubjectNameEnum.java
@@ -0,0 +1,8 @@
+package com.example.enumerate;
+
+public enum SubjectNameEnum {
+    ALL,
+    JAVA,
+    SQL,
+    MONGODB
+}
--- src/main/java/com/example/resolver/StudentResponseResolver.java
+++ src/main/java/com/example/resolver/StudentResponseResolver.java
@@ -2,6 +2,7 @@ package com.example.resolver;
 
 import com.coxautodev.graphql.tools.GraphQLResolver;
 import com.example.domain.Subjects;
+import com.example.enumerate.SubjectNameEnum;
 import com.example.response.StudentResponse;
 import com.example.response.SubjectsResponse;
 import org.springframework.stereotype.Service;
@@ -12,13 +13,17 @@ import java.util.List;
 @Service
 public class StudentResponseResolver implements GraphQLResolver<StudentResponse> {
 
-    public List<SubjectsResponse> getListSubjects (StudentResponse studentResponse) {
+    public List<SubjectsResponse> getListSubjects (StudentResponse studentResponse,
+                                                   SubjectNameEnum subjectNameEnum) {
 
-        List<SubjectsResponse> listSubjects = new ArrayList<SubjectsResponse>();
+        List<SubjectsResponse> listSubjects = new ArrayList<>();
 
-        if (studentResponse.getStudent() != null){
+        if (studentResponse.getStudent().getListSubjects() != null){
             for (Subjects sub: studentResponse.getStudent().getListSubjects()){
+                if (subjectNameEnum.name().equalsIgnoreCase(sub.getSubject()))
                 listSubjects.add(new SubjectsResponse(sub));
+                else if (subjectNameEnum.name().equalsIgnoreCase("ALL"))
+                    listSubjects.add(new SubjectsResponse(sub));
             }
         }
         return listSubjects;
